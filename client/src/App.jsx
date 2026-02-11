a401a46269f0c5a76a3d6075aa85cf6756aace5c
Zain308 <mr.zain903@gmail.com>
2026-02-11 12:22:37 +0500

    Header component is done

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import { SignIn } from "./pages/SignIn"
 import { About } from "./pages/About"
 import { Profile } from "./pages/Profile"
 import { SignUp } from "./pages/SignUp"
+import { Header } from "./components/Header"
 
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
     <BrowserRouter>
+    <Header />
       <Routes >
         <Route path="/" element={<Home />}/>
         <Route path="/sign-in" element={<SignIn />}/>
