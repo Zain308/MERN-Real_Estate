16c2092c7225313b53d599d51b703fca9ae21b42
Zain308 <mr.zain903@gmail.com>
2026-02-11 13:51:11 +0500

    connect to db

--- api/index.js
+++ api/index.js
@@ -1,5 +1,10 @@
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => console.log("The database is connected"))
+  .catch((e) => console.log(e));
 
-app.listen(3000,()=>console.log("server is running at port 3000"))
+app.listen(3000, () => console.log("server is running at port 3000"));
