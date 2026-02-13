7bd89082b09c41bc987c429acd4b27c20c3eeb1b
Zain308 <mr.zain903@gmail.com>
2026-02-13 12:58:01 +0500

    complete signup page

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ import authRouter from "./routes/auth.route.js";
 dotenv.config();
 
 const app = express();
-
+app.use(express.json())
 
 
 mongoose
@@ -20,12 +20,12 @@ app.listen(3000, () => console.log("server is running at port 3000"));
 
 app.use('/api/user',userRouter)
 app.use('/api/auth',authRouter)
-app.use(express.json())
-app.use((err,req,res,next)=>{
+
+app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
-  const message = err.message | "Internal server error";
+  const message = err.message || "Internal server error"; 
   return res.status(statusCode).json({
-    success:false,
+    success: false,
     statusCode,
     message,
   });
