3f37dd84d221bdbafa99ab8b9b13b1b8e807a5ff
Zain308 <mr.zain903@gmail.com>
2026-02-17 16:38:01 +0500

    create user update api

--- api/index.js
+++ api/index.js
@@ -3,24 +3,30 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
 const app = express();
-app.use(express.json())
 
+// Middleware
+app.use(express.json());
+app.use(cookieParser());
 
+// Database Connection
 mongoose
   .connect(process.env.MONGO)
   .then(() => console.log("The database is connected"))
-  .catch((e) => console.log(e));
+  .catch((e) => console.log("Database connection error:", e));
 
+// Server Start
 app.listen(3000, () => console.log("server is running at port 3000"));
 
+// Routes
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
-app.use('/api/user',userRouter)
-app.use('/api/auth',authRouter)
-
+// Error Handling Middleware
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
   const message = err.message || "Internal server error"; 
