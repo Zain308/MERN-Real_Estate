245e9a63cb9a838bf4d553a97be01119ce862d96
Zain308 <mr.zain903@gmail.com>
2026-02-24 11:33:45 +0500

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
 
 export const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800">About Zain's Estate</h1>
+      <p className="mb-4 text-slate-700 ">
+        Zain's Estate is a premier real estate platform that helps users find
+        their perfect home. We offer a wide range of properties for sale and
+        rent, with detailed listings and high-quality images to help you make
+        informed decisions.
+      </p>
+      <p className="mb-4 text-slate-700 ">Our mission is to simplify the home buying and renting process by providing a comprehensive platform that connects users with their ideal properties.</p>
+      <p className="mb-4 text-slate-700 ">Our team of experienced real estate professionals is dedicated to helping you find the perfect place to call home.</p>
+    </div>
+  );
+};
